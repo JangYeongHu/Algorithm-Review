88c2427a2f802b3b1a4fad098e2240fae07bf8c5
Jang-Je <128132449+Jang-Je@users.noreply.github.com>
2024-07-23 17:30:32 +0900

    [Silver V] Title: 카드1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/2161. 카드1/카드1.cc
@@ -0,0 +1,16 @@
+#include <queue>
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	queue<int> q;
+	cin >> n;
+	for (int i = 1; i <= n; i++) q.push(i);
+	while (!q.empty()) {
+		cout << q.front() << " ";
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+	return 0;
+}
