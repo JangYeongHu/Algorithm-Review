36d644dd125141d4066cc1ae1a6365b28df1616b
장영후 <128132449+JangYeongHu@users.noreply.github.com>
2025-03-16 16:20:54 +0900

    [Gold I] Title: 커피숍2, Time: 108 ms, Memory: 5928 KB -BaekjoonHub

--- C++17/백준/Gold/1275. 커피숍2/커피숍2.cc
+++ C++17/백준/Gold/1275. 커피숍2/커피숍2.cc
@@ -1,22 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long tree[100010 * 4];
+long long arr[100001];
+long long tree[100001 * 4];
+
+long long init(int start, int end, int node) {
+	if (start == end) return tree[node] = arr[start];
+	int mid = (start + end) / 2;
+	return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+}
 
 long long get(int start, int end, int node, int a, int b) {
-	if (b < start || a > end) return 0;
 	if (a <= start && b >= end) return tree[node];
+	if (b < start || a > end) return 0;
 	int mid = (start + end) / 2;
 	return get(start, mid, node * 2, a, b) + get(mid + 1, end, node * 2 + 1, a, b);
 }
 
-void update(int start, int end, int node,int ind, long long val) {
-	if (ind < start || ind > end) return;
-	if (start == end) { tree[node] = val; return; }
+long long update(int start, int end, int node, int ind) {
+	if (ind < start || ind > end) return tree[node];
+	if (start == end) { return tree[node] = arr[ind]; }
 	int mid = (start + end) / 2;
-	update(start, mid, node * 2, ind, val);
-	update(mid + 1, end, node * 2 + 1, ind, val);
-	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	return tree[node] = update(start, mid, node * 2, ind) + update(mid+1, end, node * 2 + 1, ind);
 }
 
 int main() {
@@ -24,18 +29,14 @@ int main() {
 	ios::sync_with_stdio(0);
 	int n, q;
 	cin >> n >> q;
-	for (int i = 0; i < n; i++)
-	{
-		long long a;
-		cin >> a;
-		update(1, n, 1, i+1,a);
-	}
+	for (int i = 0; i < n; i++) cin >> arr[i + 1];
+	init(1, n, 1);
 	while (q-- > 0) {
-		int x, y, a;
-		long long b;
+		int x, y, a,b;
 		cin >> x >> y >> a >> b;
 		if (x > y) swap(x, y);
 		cout << get(1, n, 1, x, y) << "\n";
-		update(1, n, 1, a,b);
+		arr[a] = b;
+		update(1, n, 1, a);
 	}
 }
