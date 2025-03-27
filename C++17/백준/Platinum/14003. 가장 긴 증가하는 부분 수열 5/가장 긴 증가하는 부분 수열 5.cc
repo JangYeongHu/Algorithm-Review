e7dcc10052a8c346a60b0c5764dacff230789326
장영후 <128132449+JangYeongHu@users.noreply.github.com>
2025-03-27 15:56:48 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 224 ms, Memory: 14476 KB -BaekjoonHub

--- C++17/백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ C++17/백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,35 +1,42 @@
-#include <cstdio>
-int t_in[1000002];
-int arr[1000002];
-int X[1000002];
+#include <bits/stdc++.h>
+using namespace std;
+int arr[1'000'001],x[1'000'001];
+
 int main() {
-	int n;
-	scanf("%d", &n);
-	for (int i = 1; i < n + 2; i++) arr[i] = 1 << 30;
-	arr[0] = 1 << 31;
-	for (int i = 1; i <= n; i++) scanf("%d", t_in + i);
-	for (int i = 0; i < n; i++)
-	{
-		int t, ind = 1 << 30;
-		t = t_in[i + 1];
-		int start = 0, end = n + 1, mid;
-		while (start < end) {
+	cin.tie(0); ios::sync_with_stdio(0);
+	int n,sz = 0;
+	cin >> n;
+	stack<int> st,res;
+	for (int i = 0; i < n; i++) x[i + 1] = 1 << 30;
+	for (int i = 0; i < n; i++) {
+		int start = 0, end = n, mid,mn = end;
+		cin >> arr[i+1];
+		while (start <= end) {
 			mid = (start + end) / 2;
-			if (arr[mid] >= t) end = mid;
-			else start = mid + 1;
+			if (x[mid] >= arr[i + 1]) {
+				end = mid - 1;
+				if (mn > mid) mn = mid;
+			}
+			else {
+				start = mid + 1;
+			}
 		}
-		arr[end] = t;
-		X[i + 1] = end;
+		x[mn] = arr[i + 1];
+		sz = max(sz, mn);
+		st.push(mn);
 	}
-	int res, ind;
-	for (int i = 1; i < n + 2; i++)
-		if (arr[i] == 1 << 30) {
-			res = i - 1;
-			break;
+	int idx = st.size();
+	while (idx>0) {
+		if (st.top() == sz) {
+			res.push(arr[idx]);
+			sz--;
 		}
-	ind = res;
-	for (int i = n; i >= 0 && ind > 0; i--)
-		if (X[i] == ind) { arr[ind] = t_in[i]; ind--; }
-	printf("%d\n", res);
-	for (int i = 1; i <= res; i++) printf("%d ", arr[i]);
+		st.pop();
+		idx--;
+	}
+	cout << res.size() << "\n";
+	while (!res.empty()) {
+		cout << res.top() << " ";
+		res.pop();
+	}
 }
