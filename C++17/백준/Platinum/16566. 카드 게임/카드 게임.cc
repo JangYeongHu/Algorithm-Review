23865c49dd3a9b29e1ae11ce88696a82aee36bcb
장영후 <128132449+JangYeongHu@users.noreply.github.com>
2025-03-27 15:06:34 +0900

    [Platinum V] Title: 카드 게임, Time: 776 ms, Memory: 33272 KB -BaekjoonHub

--- C++17/백준/Platinum/16566. 카드 게임/카드 게임.cc
+++ C++17/백준/Platinum/16566. 카드 게임/카드 게임.cc
@@ -1,46 +1,29 @@
-#include <iostream>
-#include <algorithm>
-#define MAX 4000001
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int card[MAX];
-int parent[MAX];
-int height[MAX];
-
-int find(int x) {
-	while (parent[x] != x) x = parent[x];
-	return x;
-}
-
-void tree_union(int a) {
-	a = find(a);
-	cout << card[a] << "\n";
-	parent[a] = a + 1;
-}
+int arr[4'000'001], parent[4'000'001];
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(0); ios::sync_with_stdio(0);
 	int n, m, k;
 	cin >> n >> m >> k;
-	for (int i = 0; i < m; i++) cin >> card[i];
-	sort(card, card + m);
-	for (int i = 1; i <= n; i++) parent[i] = i;
+	for (int i = 1; i <= m; i++) { cin >> arr[i]; parent[i] = i; }
+	sort(arr + 1, arr + m + 1);
 	for (int i = 0; i < k; i++) {
-		int number;
-		cin >> number;
-		int start = 0, end = m-1, mid;
+		int num;
+		cin >> num;
+		int start = 0, end = m, mid;
 		int mx = end;
 		while (start <= end) {
 			mid = (start + end) / 2;
-			if (card[mid] > number) {
-				if (card[mx] > card[mid]) mx = mid;
+			if (arr[mid] > num) {
+				if (arr[mx] > arr[mid]) mx = mid;
 				end = mid - 1;
 			}
 			else start = mid + 1;
 		}
-		tree_union(mx);
+		while (parent[mx] != mx) mx = parent[mx];
+		cout << arr[mx] << "\n";
+		parent[mx] = mx + 1;
+
 	}
-	return 0;
 }
