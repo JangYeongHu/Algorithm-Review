d9dd7817c5956ec1497acab6183ec42fc442016c
장영후 <128132449+JangYeongHu@users.noreply.github.com>
2025-05-13 16:47:30 +0900

    [Platinum III] Title: 수열과 쿼리 3, Time: 276 ms, Memory: 25884 KB -BaekjoonHub

---
+++ C++17/백준/Platinum/13544. 수열과 쿼리 3/수열과 쿼리 3.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[100001];
+vector<int> tree[100001 * 4];
+
+void update(int s, int e, int n) {
+	if (s == e) {
+		tree[n].push_back(arr[s]);
+		return;
+	}
+	int m = (s + e) / 2;
+	int l_n = n * 2, r_n = n * 2 + 1, l_ind = 0, r_ind = 0;
+	update(s, m, l_n);
+	update(m+1, e, r_n);
+	while (l_ind < tree[l_n].size() && r_ind < tree[r_n].size()) {
+		if (tree[l_n][l_ind] < tree[r_n][r_ind]) {
+			tree[n].push_back(tree[l_n][l_ind++]);
+		}
+		else {
+			tree[n].push_back(tree[r_n][r_ind++]);
+		}
+	}
+	while (l_ind < tree[l_n].size()) tree[n].push_back(tree[l_n][l_ind++]);
+	while (r_ind < tree[r_n].size()) tree[n].push_back(tree[r_n][r_ind++]);
+}
+
+int get(int s, int e, int n, int l, int r, int k) {
+	if (e < l || s > r) return 0;
+	if (s >= l && e <= r) {
+		int start = 0, end = tree[n].size()-1, mid = 0;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (tree[n][mid] == k) return tree[n].size() - mid - 1;
+			else if (tree[n][mid] < k) start = mid + 1;
+			else end = mid - 1;
+		}
+		return tree[n].size() - start;
+	}
+	int m = (s + e) / 2;
+	return get(s, m, n*2, l, r, k) + get(m + 1, e, n*2+1, l, r, k);
+}
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(0);
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) cin >> arr[i+1];
+	update(1, n, 1);
+	int m; cin >> m;
+	int ans = 0;
+	while (m--) {
+		int a, b, c; cin >> a >> b >> c;
+		a ^= ans; b ^= ans; c ^= ans;
+		ans = get(1, n, 1, a, b, c);
+		cout << ans << "\n";
+	}
+	return 0;
+}
