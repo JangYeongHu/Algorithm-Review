0a0ce6ffb5626c4cfc532dc7f74a38b64ae7a3d3
장영후 <128132449+JangYeongHu@users.noreply.github.com>
2025-06-29 02:28:36 +0900

    [Silver V] Title: 정보 선생님의 야망, Time: 876 ms, Memory: 6904 KB -BaekjoonHub

---
+++ C++17/백준/Silver/28238. 정보 선생님의 야망/정보 선생님의 야망.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool arr[1000'000][5];
+int main() {
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 5; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	int mx = 0,a = 0,b = 1;
+	for (int i = 0; i < 4; i++)
+		for (int j = i + 1; j < 5; j++) {
+			int s = 0;
+			for (int k = 0; k < n; k++)
+				if (arr[k][i] && arr[k][j]) s++;
+			if (s > mx) {
+				mx = s; a = i; b = j;
+			}
+		}
+
+	cout << mx << "\n";
+	for (int i = 0; i < 5; i++)
+		if (a == i || b == i) cout << "1 ";
+		else cout << "0 ";
+
+	return 0;
+}
