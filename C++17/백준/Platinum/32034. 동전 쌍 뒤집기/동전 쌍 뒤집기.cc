6dc9ce1815b9cf1b022bf521910d7960598fcf3f
장영후 <128132449+JangYeongHu@users.noreply.github.com>
2025-06-29 15:08:32 +0900

    [Platinum V] Title: 동전 쌍 뒤집기, Time: 320 ms, Memory: 4916 KB -BaekjoonHub

---
+++ C++17/백준/Platinum/32034. 동전 쌍 뒤집기/동전 쌍 뒤집기.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long ans() {
+	int n; cin >> n;
+	string s; cin >> s;
+	stack<int> st;
+	long long res = 0;
+	for (int i = 0; i < n; i++) {
+		if (s[i] == 'T') {
+			if (!st.empty() && st.top() % 2 != i % 2) {
+				res += i - st.top();
+				st.pop();
+			}
+			else {
+				st.push(i);
+			}
+		}
+	}
+	return st.empty()?res:-1;
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		cout << ans() << "\n";
+	}
+	return 0;
+}
