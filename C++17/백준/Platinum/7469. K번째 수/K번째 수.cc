1cc9389d0817309de7533c6c59ad50af3c21f2c2
장영후 <128132449+JangYeongHu@users.noreply.github.com>
2025-08-12 11:19:44 +0900

    [Platinum II] Title: K번째 수, Time: 348 ms, Memory: 23264 KB -BaekjoonHub

--- C++17/백준/Platinum/7469. K번째 수/K번째 수.cc
+++ C++17/백준/Platinum/7469. K번째 수/K번째 수.cc
@@ -9,55 +9,43 @@ void init(int s, int e, int n) {
 		return;
 	}
 	int m = (s + e) / 2;
-	int l_n = n * 2, r_n = l_n + 1;
-	int lind = 0, rind = 0;
-	init(s, m, l_n);
-	init(m + 1, e, r_n);
-	auto larr = tree[l_n];
-	auto rarr = tree[r_n];
-	while (larr.size() > lind && rarr.size() > rind) {
-		if (larr[lind] < rarr[rind]) {
-			tree[n].push_back(larr[lind++]);
-		}
-		else {
-			tree[n].push_back(rarr[rind++]);
-		}
-	}
-	while(larr.size() > lind) tree[n].push_back(larr[lind++]);
-	while (rarr.size() > rind) tree[n].push_back(rarr[rind++]);
+	int lidx = n * 2, ridx = lidx + 1;
+	init(s, m, lidx); init(m + 1, e, ridx);
+	tree[n].resize(tree[lidx].size() + tree[ridx].size());
+	merge(tree[lidx].begin(), tree[lidx].end(), tree[ridx].begin(), tree[ridx].end(), tree[n].begin());
 }
 
-int get(int s, int e, int n, int l, int r,long long k) {
-	if (r < s || l > e	) return 0;
-	if (s >= l && e <= r) {
-		int res = upper_bound(tree[n].begin(), tree[n].end(), k) - tree[n].begin();
-		return res;
-	}
+int query(int s, int e, int n,int l,int r,int k) {
+	if (e < l || s > r) return 0;
+	if (s >= l && e <= r) return upper_bound(tree[n].begin(), tree[n].end(), k) - tree[n].begin();
 	int m = (s + e) / 2;
-	return get(s, m, n * 2, l, r, k) + get(m+1, e, n * 2+1, l, r, k);
+	return query(s, m, n * 2, l, r, k) + query(m + 1, e, n * 2 + 1, l, r, k);
 }
 
 int main() {
-	cin.tie(0); ios::sync_with_stdio(0);
 	int n, m; cin >> n >> m;
-	for (int i = 0; i < n; i++) cin >> arr[i + 1];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i + 1];
+	}
 
 	init(1, n, 1);
 
-	while (m-- > 0) {
-		int a, b, k;
-		cin >> a >> b >> k;
-		long long start = -1e9,end = 1e9,mid;
-		long long ans = end;
-		while (start <= end) {
-			mid = (start + end) / 2;
-			int res = get(1, n, 1, a, b, mid);
+	while (m--) {
+		int i, j, k;
+		cin >> i >> j >> k;
+
+		int en = 1<<30, st = -en, mid, ans = en;
+		while(st <= en) {
+			mid = (st + en) / 2;
+			int res = query(1, n, 1, i, j, mid);
 			if (res >= k) {
 				ans = min(ans, mid);
-				end = mid - 1;
+				en = mid - 1;
 			}
-			else start = mid + 1;
+			else st = mid + 1;
 		}
 		cout << ans << "\n";
 	}
+
+	return 0;
 }
