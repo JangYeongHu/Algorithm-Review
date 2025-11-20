4da470147685a298c58f6c0aa180a20e08ff0e91
YeongHu Jang <128132449+JangYeongHu@users.noreply.github.com>
2025-11-21 00:39:10 +0900

    [Gold I] Title: 다리 만들기 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- C++17/백준/Gold/17472. 다리 만들기 2/다리 만들기 2.cc
+++ C++17/백준/Gold/17472. 다리 만들기 2/다리 만들기 2.cc
@@ -1,108 +1,171 @@
-#include <cstdio>
+#include <iostream>
+#include <vector>
 #include <queue>
+#include <array>
 #include <algorithm>
+
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+
 using namespace std;
-int arr[10][10];
-typedef struct { int x, y, d, i; } element;
-struct cmp {
-    bool operator()(const element& e1, element& e2) { return e1.i > e2.i; }};
-vector<pair<int, int>> land[8];
-priority_queue<element,vector<element>,cmp> pq;
-int dx[] = { 1,-1,0,0 }, dy[] = { 0,0,1,-1 };
-int parent[8],root[8];
-int n, m;
-void landbfs(int ind, int x, int y) {
-    queue<pair<int, int>> q;
-    arr[y][x] = ind;
-    q.push({ x,y });
-    land[ind].push_back({ x,y });
-    while (!q.empty()) {
-        auto t = q.front();
-        q.pop();
-        for (int i = 0; i < 4; i++) {
-            int tx = t.first + dx[i];
-            int ty = t.second + dy[i];
-            if (tx < 0 || tx >= m || ty < 0 || ty >= n) continue;
-            if (arr[ty][tx] == ind || arr[ty][tx] == 0) continue;
-            arr[ty][tx] = ind;
-            q.push({ tx,ty });
-            land[ind].push_back({ tx,ty });
+
+struct Pos
+{
+    int r, c;
+};
+
+array<int, 4> dr = { 0,1,0,-1 };
+array<int, 4> dc = { 1,0,-1,0 };
+
+void BFS(Pos s, int u, vector<vector<int>>& map)
+{
+    int n = map.size();
+    int m = map[0].size();
+
+    map[s.r][s.c] = u;
+    queue<Pos> Q;
+    Q.push(s);
+
+    while (!Q.empty())
+    {
+        auto [r, c] = Q.front();
+        Q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if (nr < 0 || nr >= n || nc < 0 || nc >= m)
+                continue;
+            if (map[nr][nc] != -1)
+                continue;
+
+            map[nr][nc] = u;
+            Q.push({ nr,nc });
         }
     }
 }
 
-void bridgebfs(int ind) {
-    queue<element> q;
-    bool vis[8] = { 0, };
-    for (auto iter = land[ind].begin(); iter != land[ind].end(); iter++) {
-        int x = (*iter).first;
-        int y = (*iter).second;
-        for (int i = 0; i < 4; i++) {
-            int tx = x + dx[i];
-            int ty = y + dy[i];
-            if (tx < 0 || tx >= m || ty < 0 || ty >= n) continue;
-            if (arr[ty][tx]) continue;
-            q.push({tx,ty,i,1});
+int Numbering(vector<vector<int>>& map)
+{
+    int n = map.size();
+    int m = map[0].size();
+
+    int k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (map[i][j] == -1)
+                BFS({ i,j }, k++, map);
         }
     }
-    while (!q.empty()) {
-        auto t = q.front();
-        q.pop();
-        int tx = t.x + dx[t.d];
-        int ty = t.y + dy[t.d];
-        if (tx < 0 || tx >= m || ty < 0 || ty >= n) continue;
-        if (vis[arr[ty][tx]]) continue;
-        if (!arr[ty][tx]) { q.push({ tx,ty,t.d,t.i + 1 }); continue; }
-        if (t.i == 1) continue;
-        vis[arr[ty][tx]] = 1;
-        pq.push({ ind,arr[ty][tx],0,t.i});
-    }
+    return k;
 }
 
-int find(int x) {
-    if (parent[x] == x) return x;
-    return find(parent[x]);
-}
+struct Edge
+{
+    int u, v, w;
 
-bool uni(int a, int b) {
-    a = find(a);
-    b = find(b);
-    if (a == b) return 1;
-    if (root[a] > root[b]) {
-        parent[b] = a;
-    }
-    else {
-        parent[a] = b;
-        if (root[a] == root[b]) root[b]++;
-    }
-    return 0;
-}
+    friend bool operator < (const Edge& a, const Edge& b) { return a.w < b.w; }
+};
 
-int mst(int ind) {
-    int res = 0,cnt = 0;
-    while (!pq.empty()) {
-        auto t = pq.top();
-        pq.pop();
-        if (uni(t.x, t.y)) continue;
-        res += t.i;
-        cnt++;
-    }
-    if (cnt == ind-3) return res;
-    return -1;
-}
+void MakeEdges(vector<vector<int>>& map, vector<Edge>& edges)
+{
+    int n = map.size();
+    int m = map[0].size();
 
-int main() {
-    scanf("%d %d", &n, &m);
     for (int i = 0; i < n; i++)
+    {
+        int c = -1;
         for (int j = 0; j < m; j++)
-            scanf("%d", &arr[i][j]);
-    int ind = 2;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            if (arr[i][j] == 1) landbfs(ind++, j, i);
-    for (int i = 2; i < ind; i++) parent[i] = i;
-    for (int i = 2; i < ind; i++) bridgebfs(i);
+        {
+            if (map[i][j] == -2)
+                continue;
+            if (c == -1 || map[i][c] == map[i][j])
+            {
+                c = j;
+                continue;
+            }
 
-    printf("%d", mst(ind));
-    return 0;
+            int w = j - c - 1;
+            if (w > 1)
+                edges.push_back({ map[i][c],map[i][j],w });
+            c = j;
+        }
+    }
+    for (int j = 0; j < m; j++)
+    {
+        int r = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (map[i][j] == -2)
+                continue;
+            if (r == -1 || map[r][j] == map[i][j])
+            {
+                r = i;
+                continue;
+            }
+
+            int w = i - r - 1;
+            if (w > 1)
+                edges.push_back({ map[r][j],map[i][j],w });
+            r = i;
+        }
+    }
+}
+
+struct UF
+{
+    vector<int> p;
+    UF(int n) { p.resize(n, -1); }
+    int Find(int u) { return p[u] < 0 ? u : p[u] = Find(p[u]); }
+    void Union(int u, int v)
+    {
+        u = Find(u), v = Find(v);
+        if (u == v)
+            return;
+        if (p[u] > p[v])
+            swap(u, v);
+        p[u] += p[v], p[v] = u;
+    }
+};
+
+int main()
+{
+    FastIO;
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> map(n, vector<int>(m));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> map[i][j];
+            map[i][j] -= 2;
+        }
+    }
+    int k = Numbering(map);
+
+    vector<Edge> edges;
+    MakeEdges(map, edges);
+    sort(edges.begin(), edges.end());
+
+    UF uf(k);
+    int result = 0;
+    for (auto& [u, v, w] : edges)
+    {
+        if (uf.Find(u) == uf.Find(v))
+            continue;
+
+        uf.Union(u, v);
+        result += w;
+    }
+
+    if (-uf.p[uf.Find(0)] != k)
+        cout << -1 << '\n';
+    else
+        cout << result << '\n';
 }
